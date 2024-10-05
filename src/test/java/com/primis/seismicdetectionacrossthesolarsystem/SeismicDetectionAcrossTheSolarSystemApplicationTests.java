5bcb05c425cc9d5a83b4283bcd8a8f5aa2b755a4
Alameen Sabbah <70842282+ZeroNeroIV@users.noreply.github.com>
2024-10-05 14:19:58 +0300

    Day 1 part 1

---
+++ src/test/java/com/primis/seismicdetectionacrossthesolarsystem/SeismicDetectionAcrossTheSolarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primis.seismicdetectionacrossthesolarsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeismicDetectionAcrossTheSolarSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
