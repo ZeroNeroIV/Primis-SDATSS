261a1adfc9ed0048ec3b2eddb8356476fc305a88
Alameen Sabbah <70842282+ZeroNeroIV@users.noreply.github.com>
2024-10-06 22:40:22 +0300

    ...to refactor

--- src/main/java/com/primis/seismicdetectionacrossthesolarsystem/dtos/ZipFileDto.java
+++
@@ -1,9 +0,0 @@
-package com.primis.seismicdetectionacrossthesolarsystem.dtos;
-
-import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
-
-@Data
-public class ZipFileDto {
-    private MultipartFile file;
-}
